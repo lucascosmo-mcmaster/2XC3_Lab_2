cc6b6c628f7d0d970c7574c4cc28f34183e6acef
Eliana Marshall <elianagmarshall@gmail.com>
2026-02-11 16:39:29 -0500

    Implement approx2 function in approx_algs.py

--- approx_algs.py
+++ approx_algs.py
@@ -1,18 +1,25 @@
-def approx1(G): 
-    C = set() #start with empty set to store the vertex cover
-    adj_cp = {} #create an empty copy of the adjacency list to modify
+import random
 
-    #copy each vertex and its neighbors into the copy of the adjacency list
+
+#helper function to copy each vertex and its neighbors into the copy of the adjacency list
+def copy_adj(G, adj_cp):
     for vertex in G.adj:
         neighbors = G.adj[vertex]
         adj_cp[vertex] = list(neighbors)
-    
-    #helper function to check if there are any edges left in a graph
-    def has_edges(adj):
-        for neighbor in adj.values():
-            if len(neighbor) > 0:
-                return True
-        return False
+
+
+#helper function to check if there are any edges left in a graph
+def has_edges(adj):
+    for neighbor in adj.values():
+        if len(neighbor) > 0:
+            return True
+    return False
+
+
+def approx1(G): 
+    C = set() #start with empty set to store the vertex cover
+    adj_cp = {} #create an empty copy of the adjacency list to modify
+    copy_adj(G, adj_cp) #copy the adjacency list into adj_cp
     
     #while there are still edges in the graph
     while has_edges(adj_cp):
@@ -33,5 +40,24 @@ def approx1(G):
         for u in adj_cp[v]:
             adj_cp[u].remove(v)
         adj_cp[v] = []
+    
+    return C
+
+
+def approx2(G):
+    C = set() #start with empty set to store the vertex cover
+    adj_cp = {} #create an empty copy of the adjacency list to modify
+    copy_adj(G, adj_cp) #copy the adjacency list into adj_cp
+
+    #while there are still edges in the graph
+    while has_edges(adj_cp):
+        vertices = [] #list of vertices not in the vertex cover
+        
+        for v in adj_cp:
+            if v not in C:
+                vertices.append(v)
+        
+        v = random.choice(vertices) #choose a random vertex from the list of vertices not in the vertex cover
+        C.add(v) #add the random vertex to the vertex cover
     
     return C
