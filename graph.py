b0a06db3dcf266a5e80933ca9f5cc91c8112e0eb
Eliana Marshall <elianagmarshall@gmail.com>
2026-02-20 23:49:45 -0500

    Fix DFS2 and add comments to BFS2 & DFS2

--- graph.py
+++ graph.py
@@ -43,6 +43,7 @@ def BFS(G, node1, node2):
                 marked[node] = True
     return False
 
+
 def walk_back(path_back, start):
     path = deque()
     current = start
@@ -52,20 +53,32 @@ def walk_back(path_back, start):
         current = path_back[current]
     return list(path)
 
+
+#Breadth First Search variant that returns the list of nodes in the path from node1 to node2
 def BFS2(G, node1, node2):
-    Q = deque([node1])
-    path = {node1 : node1} 
+    #base case where node1 and node2 are the same
+    if node1 == node2:
+        return [node1]
+    
+    Q = deque([node1]) #initialize queue with starting node
+    path = {} #dictionary to store each node's predecessor
+
+    #initialize all nodes as unvisited
     for node in G.adj: 
         if node != node1:
             path[node] = None
+    
+    path[node1] = node1 #the path from node1 points to itself
+    
+    #perform BFS until all nodes have been visited
     while len(Q) != 0: 
-        current_node = Q.popleft()
-        for node in G.adj[current_node]: 
-            if path[node] == None: 
+        current_node = Q.popleft() #remove the next node from the queue
+        for node in G.adj[current_node]: #visit all neighbors of the current node
+            if path[node] == None: #if a neighbor hasn't been visited yet, add it to the queue
                 Q.append(node) 
-                path[node] = current_node 
-                if node == node2: 
-                    walk_back(path, node)
+                path[node] = current_node #record the path
+                if node == node2: #if the target node is found, walk back to find the path again
+                    return walk_back(path, node)
     return {}
 
 
@@ -85,33 +98,42 @@ def DFS(G, node1, node2):
                 S.append(node)
     return False
 
+
+#Depth First Search variant that returns the list of nodes in the path from node1 to node2
 def DFS2 (G, node1, node2):
-    S = [node1]
-    marked = {}
-    path = {node1 : node1}
+    S = [node1] #initialize stack with starting node
+    marked = {} #dictionary to track which nodes have been visited
+    path = {} #dictionary to store each node's predecessor
+
+    #initialize all nodes as unvisited/without a path
     for node in G.adj:
         marked[node] = False
         path[node] = None
+    
+    marked[node1] = True #the starting node is marked
+    path[node1] = node1 #the path from node1 points to itself
+
+
+    #perform DFS until all nodes have been visited
     while len(S) != 0:
-        current_node = S.pop()
-        if not marked[current_node]:
-            marked[current_node] = True
-            for node in G.adj[current_node]:
-                if node == node2:
-                    path[node] = current_node #TODO:some sort of order issue im too lazy to comprehend right now
-                    return walk_back(path, node)
+        current_node = S.pop() #pop the current node from the stack
+        for node in G.adj[current_node]: #visit all neighbors of the current node
+            if not marked[node]: #if a node hasn't been marked yet, add it to the stack
                 S.append(node)
-                if path[node] is None:
-                    path[node] = current_node
+                marked[node] = True #mark the node as visited
+                path[node] = current_node #record the path
+                if node == node2: #if the target node is found, walk back to find the path again
+                    return walk_back(path, node)
     return {}
 
-#Use the methods below to determine minimum vertex covers
 
+#Use the methods below to determine minimum vertex covers
 def add_to_each(sets, element):
     copy = sets.copy()
     for set in copy:
         set.append(element)
     return copy
+
 
 def power_set(set):
     if set == []:
@@ -125,6 +147,7 @@ def is_vertex_cover(G, C):
                 return False
     return True
 
+
 def MVC(G):
     nodes = [i for i in range(G.get_size())]
     subsets = power_set(nodes)
@@ -134,5 +157,3 @@ def MVC(G):
             if len(subset) < len(min_cover):
                 min_cover = subset
     return min_cover
-
-
