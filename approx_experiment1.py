09e6ab9496a54d6df4a9809365b2b720171d9e5b
Eliana Marshall <elianagmarshall@gmail.com>
2026-02-21 19:23:55 -0500

    Perform approx_experiment1 on 1000 trials instead of 100

--- approx_experiment1.py
+++ approx_experiment1.py
@@ -2,6 +2,6 @@ from experiments import plot_graph, approx_experiment1
 
 N = 8
 MAX_EDGES = 30
-TRIALS = 100
+TRIALS = 1000
 
 plot_graph(N, MAX_EDGES, TRIALS, "approx_experiment1", approx_experiment1, "Average Ratio to MVC")
